f634fa116a679563a1b15f6bb77ab81c8e55ad4e
Brandon Murry <murry005@rangers.uwp.edu>
2023-12-30 02:09:24 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalBugFix.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalBugFix
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Murry.LethalBugFix";
+        private const string modName = "Lethal Bug Fix";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModBase instance;
+
+        ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            logSource.LogInfo(modName + " has AWOKEN!");
+
+            harmony.PatchAll(typeof(ModBase));
+            harmony.PatchAll(typeof(LightningPatch));
+        }
+    }
+}
---
+++ Patches/LightningPatch.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LethalBugFix.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class LightningPatch
+    {
+        [HarmonyPatch("GrabObject")]
+        [HarmonyPostfix]
+        static void deathByObject(ref int ___health, ref PlayerControllerB __instance, ref GrabbableObject ___currentlyGrabbingObject)
+        {
+            if (___currentlyGrabbingObject == null) //LITERALLY REPLACE NULL WITH BEHIVE AND THIS SHOULD WORK! :)
+            {
+                ManualLogSource logSource = BepInEx.Logging.Logger.CreateLogSource("afsdghsertfdhbersthn");
+                logSource.LogInfo("We trying ta kill ya.");
+                __instance.KillPlayer(Vector3.zero);
+            }
+        }
+    }
+}
